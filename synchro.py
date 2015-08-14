36a6def782786b0de094bc9df1c70c442d3e48b0
Navy Cheng <navych@126.com>
2015-08-14 16:05:42 +0800

    Synchro.py: improve the arguments parse system by using *argparse*
    The previous args parsing is ungly and not standard. Use the python
    built-in moudle argparse to improve that.
    
    Signed-off-by: Navy Cheng <navych@126.com>

--- synchro.py
+++ synchro.py
@@ -5,6 +5,7 @@ You can ignore this script. This script is to be improved.
 '''
 import shutil
 import os
+import argparse
 
 HOME=r'/home/navy'
 
@@ -29,4 +30,20 @@ def copyFiles(source, dest):
         shutil.copy(sfile, dfile)
         print("... copy: %s ---->  %s"%(sfile, dfile))
 
-copyFiles(files2, files1)
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="Install and config this repository.")
+    group = parser.add_mutually_exclusive_group()
+
+    update_help="update the scripts in this repository if the installed scripts changed."
+    install_help="install the scripts in this repository."
+    group.add_argument("-u", "--update", help=update_help, action="store_true")
+    group.add_argument("-i", "--install", help=install_help, action="store_true")
+    args = parser.parse_args()
+    
+    if args.update:
+        print("...update:")
+        copyFiles(files2,files1)
+    elif args.install:
+        print("...install:")
+        copyFiles(files1, files2)
